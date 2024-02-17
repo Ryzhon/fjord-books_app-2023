7d3134c68bb1b1a226ba194ce1ea8c1b6fcf793a
Ryzhon <ryuta.hokazono@gmail.com>
2024-02-17 14:21:03 +0900

    add: devise

--- Gemfile
+++ Gemfile
@@ -43,6 +43,10 @@ gem 'tzinfo-data', platforms: %i[mingw mswin x64_mingw jruby]
 
 # Reduces boot times through caching; required in config/boot.rb
 gem 'bootsnap', require: false
+gem 'devise'
+gem 'devise-i18n-views'
+gem 'dotenv'
+gem 'kaminari'
 gem 'rails-i18n'
 
 # Use Sass to process CSS
--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,14 @@
 # frozen_string_literal: true
 
 class ApplicationController < ActionController::Base
+  before_action :configure_permitted_parameters, if: :devise_controller?
+
+  protected
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: %i[username postal_code address])
+
+    # アカウント編集時にパスワードなしで更新を許可
+    devise_parameter_sanitizer.permit(:account_update, keys: %i[username email postal_code address introduction])
+  end
 end
