5d69640961f23f52e2ee80426735418293788e99
Ryzhon <ryuta.hokazono@gmail.com>
2024-03-13 14:17:38 +0900

    fix: 保存するデータが表示されるかのアサーションを追加

--- test/models/report_test.rb
+++ test/models/report_test.rb
@@ -8,20 +8,6 @@ class ReportTest < ActiveSupport::TestCase
     @report = @user.reports.build(title: 'テスト日報', content: '日報の内容をここに記載する。')
   end
 
-  test '保存される' do
-    assert @report.valid?
-  end
-
-  test 'タイトルがないとエラーになる' do
-    @report.title = ''
-    assert_not @report.valid?
-  end
-
-  test '内容がないとエラーになる' do
-    @report.content = ''
-    assert_not @report.valid?
-  end
-
   test '作成したユーザーが編集することができる' do
     assert @report.editable?(@user)
   end
@@ -31,23 +17,13 @@ class ReportTest < ActiveSupport::TestCase
     assert_not @report.editable?(other_user)
   end
 
-  test '関連するコメントも一緒に削除される' do
-    @report.save
-    @report.comments.create!(content: 'テストコメント', user: @user)
-
-    assert_difference 'Report.count', -1 do
-      assert_difference 'Comment.count', -1 do
-        @report.destroy
-      end
-    end
-  end
-
   test 'created_onで曜日の形式を変更する' do
-    @report.created_at = Time.zone.now
-    assert_equal @report.created_on, Time.zone.today
+    current_time = Time.zone.now
+    @report.created_at = current_time
+    assert_equal @report.created_on, current_time.to_date
   end
 
-  test 'save_mentionsでコンテントから日報のメンションを保存できている' do
+  test 'save_mentionsでcontentから日報のメンションを保存できている' do
     mentioned_report1 = @user.reports.create!(title: 'メンションされるレポート1', content: 'これはテストレポートです。')
     mentioned_report2 = @user.reports.create!(title: 'メンションされるレポート2', content: 'これもテストレポートです。')
 
--- test/system/reports_test.rb
+++ test/system/reports_test.rb
@@ -21,20 +21,25 @@ class ReportsTest < ApplicationSystemTestCase
     fill_in '内容', with: @report.content
     fill_in 'タイトル', with: @report.title
     click_on '登録する'
-
     assert_text '日報が作成されました。'
+    assert_text @report.title
+    assert_text @report.content
     click_on '日報の一覧に戻る'
   end
 
   test 'should update Report' do
+    updated_title = '更新されたタイトル'
+    updated_content = '更新された内容'
     visit report_url(@report)
     click_on 'この日報を編集', match: :first
 
-    fill_in '内容', with: @report.content
-    fill_in 'タイトル', with: @report.title
+    fill_in '内容', with: updated_title
+    fill_in 'タイトル', with: updated_content
     click_on '更新する'
 
     assert_text '日報が更新されました。'
+    assert_text updated_title
+    assert_text updated_content
     click_on '日報の一覧に戻る'
   end
 
@@ -43,5 +48,7 @@ class ReportsTest < ApplicationSystemTestCase
     click_on 'この日報を削除', match: :first
 
     assert_text '日報が削除されました。'
+    assert_no_text @report.title
+    assert_no_text @report.content
   end
 end
