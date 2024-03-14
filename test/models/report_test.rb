2aadb58e9681eb15849489fca19f0499f1f45570
Ryzhon <ryuta.hokazono@gmail.com>
2024-03-14 09:01:46 +0900

    fix: 日報のモデルテストで更新された時にメンション数も変更できるかのテストも追加する

--- test/models/report_test.rb
+++ test/models/report_test.rb
@@ -36,4 +36,32 @@ class ReportTest < ActiveSupport::TestCase
     assert_includes @report.mentioning_reports, mentioned_report1
     assert_includes @report.mentioning_reports, mentioned_report2
   end
+  test 'save_mentionsでcontentから日報のメンションを保存して更新時に変更されること' do
+    mentioned_report1 = @user.reports.create!(title: 'メンションされるレポート1', content: 'これはテストレポートです。')
+    mentioned_report2 = @user.reports.create!(title: 'メンションされるレポート2', content: 'これもテストレポートです。')
+    new_mentioned_report = @user.reports.create!(title: '新しくメンションされるレポート', content: '新しいテストレポートです。')
+
+    @report.content = "これはテストです。メンションされるレポートはこちらです: http://localhost:3000/reports/#{mentioned_report1.id} と http://localhost:3000/reports/#{mentioned_report2.id}"
+    assert_difference 'ReportMention.count', 2 do
+      @report.save
+    end
+    assert_includes @report.mentioning_reports, mentioned_report1
+    assert_includes @report.mentioning_reports, mentioned_report2
+
+    @report.content = "内容が更新されました。新しいメンション: http://localhost:3000/reports/#{new_mentioned_report.id}"
+    assert_difference 'ReportMention.count', -1 do
+      @report.save
+    end
+    @report.reload
+    assert_not_includes @report.mentioning_reports, mentioned_report1
+    assert_not_includes @report.mentioning_reports, mentioned_report2
+    assert_includes @report.mentioning_reports, new_mentioned_report
+
+    @report.content = "内容がさらに更新されました。新しいメンション: http://localhost:3000/reports/#{mentioned_report1.id} と http://localhost:3000/reports/#{mentioned_report2.id}"
+    assert_difference 'ReportMention.count', 1 do
+      @report.save
+    end
+    assert_includes @report.mentioning_reports, mentioned_report1
+    assert_includes @report.mentioning_reports, mentioned_report2
+  end
 end
